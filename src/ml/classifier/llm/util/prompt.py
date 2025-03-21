9df18fc44dc5365efd887cf032e148e82a4ac394
Alexander Grote <alexandergrote.ag@gmail.com>
2025-03-21 10:46:00 +0100

    refactor(src/ml/classifier/llm/util/prompt.py): simplify and refactor prompt creation logic

--- src/ml/classifier/llm/util/prompt.py
+++ src/ml/classifier/llm/util/prompt.py
@@ -1,83 +1,44 @@
 from pydantic import BaseModel, field_validator
 from pydantic.config import ConfigDict
 from typing import Any, List, Dict
-from langchain_core.prompts.few_shot import FewShotPromptTemplate, PromptTemplate
-        
-        
-class FewshotPromptCreator(BaseModel):
+from langchain.output_parsers import PydanticOutputParser
+from src.constants import Directory
 
-    parser: Any
 
-    prefix_prompt: str 
-    suffix_prompt: str
+CHAIN_OF_THOUGHT = "Let's think step by step"
+UNKNOWN_CLASS_LABEL = UnknownClassLabel.UNKNOWN_STR.value
 
-    text_to_classify: str
-    examples: List[Dict[str, str]]
 
-    model_config = ConfigDict(arbitrary_types_allowed=True)
+def create_prompt(
+    template: str, 
+    text_to_classify: str, 
+    available_classes: List[str], 
+    examples: List[Dict[str, str]], 
+    use_classes_in_examples: bool, 
+    parser: PydanticOutputParser,
+    ) -> str:
 
-    @staticmethod
-    def get_chain_input_field_name() -> str:
-        return "query"
-    
-    @staticmethod
-    def get_format_instruction_field() -> str:
-        return "format_instructions"
+    instructions = parser.get_format_instructions()
 
-    @field_validator("prefix_prompt")
-    @classmethod
-    def validate_prefix_prompt(cls, v):
+    classes_msg = '\n'.join([el["output"] for el in examples])
 
-        field = "{" + cls.get_chain_input_field_name() + "}"
-        assert field in v, f"prefix_prompt must contain the field {field}"
+    if use_classes_in_examples:
+        examples_msg = '\n'.join([f"{example['input']} -> {example['output']}" for example in examples])
+    else:
+        examples_msg = '\n'.join([f"{example['input']}" for example in examples])
 
-        return v
-    
-    @field_validator("suffix_prompt")
-    @classmethod
-    def validate_suffix(cls, v):
 
-        field = "{" + cls.get_format_instruction_field() + "}"
-        assert field in v, f"suffix_prompt must contain the field {field}"
+    prompt = template.format(
+        examples_msg=examples_msg,
+        text=text,
+        instructions=instructions,
+        classes_msg=classes_msg
+    )
 
-        return v
+    return prompt
 
-    @field_validator("examples")
-    @classmethod
-    def validate_examples(cls, v):
+if __name__ == '__main__':
 
-        for example in v:
-            assert "input" in example
-            assert "output" in example
+    for file in 
 
-        return v
-    
-    @field_validator("parser")
-    @classmethod
-    def validate_parser(cls, v):
-
-        assert hasattr(v, "get_format_instructions")
-
-        return v
-    
-
-    def create(self) -> str:
-
-        instructions = self.parser.get_format_instructions()
-
-        example_prompt = PromptTemplate(
-            template="{input} -> {output}",
-            input_variables=["input", "output"]
-        )
-        
-        prompt = FewShotPromptTemplate(
-            prefix=self.prefix_prompt,
-            suffix=self.suffix_prompt,
-            examples=self.examples,
-            example_prompt=example_prompt,
-            input_variables=[FewshotPromptCreator.get_chain_input_field_name()],
-            partial_variables={FewshotPromptCreator.get_format_instruction_field(): instructions},
-            validate_template=True
-        )
-
-        return prompt.format(**{FewshotPromptCreator.get_chain_input_field_name(): self.text_to_classify}) 
+    text = "example text"
