fcef09595a6146dc8613f0188b45f1d22316a1ac
Alexander Grote <alexandergrote.ag@gmail.com>
2025-03-29 16:42:00 +0100

    updating mlflow deletion

--- src/util/mlflow_util.py
+++ src/util/mlflow_util.py
@@ -1,6 +1,17 @@
+import os
 import mlflow
 from mlflow import MlflowClient
+from pathlib import Path
 from src.util.constants import Directory
+
+
+def get_tracking_uri():
+
+    if os.name == 'nt':
+        return Path(rf"file:\\{str(Directory.ROOT)}\mlruns")
+    
+    return Path(rf"file:{str(Directory.ROOT)}/mlruns")
+
 
 def get_mlflow_client() -> MlflowClient:
 
