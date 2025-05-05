2f771c45a8dfacce18de7b3d808b4061bdfa0cb4
Alexander Grote <alexandergrote.ag@gmail.com>
2025-05-05 17:18:41 +0200

    refactor(run/run.sh): update script paths to use relative directory

--- run/run.sh
+++ run/run.sh
@@ -5,14 +5,14 @@ NUM_SHORT_PROCESSES=5
 
 # Run the memory-intensive process first
 echo "Starting memory-intensive process..."
-python long_process.py &
+python run/long_process.py &
 LONG_PID=$!
 
 # Run multiple short processes
 echo "Starting $NUM_SHORT_PROCESSES short processes..."
 for i in $(seq 1 $NUM_SHORT_PROCESSES); do
     echo "Starting short process $i..."
-    python short_process.py &
+    python run/short_process.py &
     SHORT_PIDS[$i]=$!
 done
 
